2528f94a375694973e5294863b098b8eb5120e9c
onsokumaru <11082686+onsokumaru@users.noreply.github.com>
2023-11-16 15:51:29 -0500

    adding functions

---
+++ modules/converse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def hello_world():
+    return f"Hello, World!"
+
+def hello():
+    return f"Hello, nice to meet you!"
+
+def greet(name: str):
+    return f"Hello, {name}!"
+
+def bye():
+    return f"Goodbye, see you later."
